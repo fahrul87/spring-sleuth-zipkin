e0cb81941a380ce260128e4f35de7bcdbb44b5fd
Fahrul Arya Sugita <faryasugita@gmail.com>
2020-06-17 14:16:18 +0700

    istimiwir

---
+++ payment-service.zipkin/src/test/java/com/fahrul/paymentservice/zipkin/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fahrul.paymentservice.zipkin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
